eab786c07b8cb0da03ba1fa942b18853e5a20444
Priyansh Patidar <priyanshpatidar@Priyanshs-MacBook-Pro.local>
2025-07-22 16:21:48 +0530

    change the root url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://sip-algo-trading-system.vercel.app',
+      '/api': 'https://sip-algo-trading-system.onrender.com',
     },
   },
 })
